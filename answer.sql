92b775f64dc2a985453f04c4b9407d1073bd3da1
bilwangw <billandoel123@gmail.com>
2025-11-24 17:23:22 -0500

    added sql

--- answer.sql
+++ answer.sql
@@ -2,12 +2,15 @@
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
 
+-- 1527912000 UTC timestamp for 2018-06-02
 
+select count(id) from login_history where login_timestamp > 1527912000;
+select count(id) from login_history where login_timestamp <= 1527912000;
 
 
 -- PART 2: Create a SQL query that indicates the number of status changes by card
 
-
+select cardID, count(id) from card_change_history group by cardID;
 
 
 
